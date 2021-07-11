62db63c046b67b66223dbbba243f721e33f3dae4
zhaoxingming <zhaoxingming@nucarf.com>
2021-07-11 23:37:01 +0800

    del: 删除无用代码

--- src/api/index.js
+++
@@ -1,4 +0,0 @@
-const axios = require('./config');
-
-// 从gitlab获取脚手架项目列表数据
-exports.getFeProjectList = (params) => axios.get('/groups/266/projects?search=-template', params);
--- src/api/npm.js
+++
@@ -1,4 +0,0 @@
-const axios = require('./config');
-
-// 获取最新版本
-exports.getVersions = () => axios.get('http://npm.nucarf.cn/-/verdaccio/sidebar/nucarf-cli');

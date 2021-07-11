c75210fcaeadfda411f916d00242eade52d3fe66
zhaoxingming <zhaoxingming@nucarf.com>
2021-07-11 23:36:42 +0800

    feat: 文案修改

--- src/commandList.js
+++ src/commandList.js
@@ -2,11 +2,11 @@ module.exports = [
     {
         command: 'create <app-name>',
         commandFileName: 'create',
-        description: '输入项目名称，初始化项目',
+        description: '输入项目名称，拉取项目基础模板',
     },
     {
         command: 'list',
         commandFileName: 'list',
-        description: '列出所有脚手架模板',
+        description: '列出所有项目脚手架模板',
     },
 ];
